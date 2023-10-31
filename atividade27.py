210c7cf63970442d8a19f2b905919632a0d4f60c
Luizyy <analuizynascimentodasilva@gmail.com>
2023-10-31 16:47:24 -0300

    polindromo

---
+++ atividade27.py
@@ -0,0 +1,13 @@
+# Exercício Python 27: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo,
+# desconsiderando os espaços. DICA: VEJA SOBRE FUNÇÃO REPLACE(), LOWER() E UMA FORMA DE INVERTER OS CARACTERES. 
+
+
+frase = str(input('Digite a frase:'))
+frase = frase.upper().replace(" ","") 
+invertido = frase.upper() [::-1] 
+
+invertido = invertido.replace(" ", "")
+if frase == invertido:
+    print("palíndromo")
+else:
+    print("não é palíndromo")
